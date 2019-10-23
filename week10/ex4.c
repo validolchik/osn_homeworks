3a53b69c78e4f6e6bc8b99e19884d75fa5f86159
validolchik <valik.valeevrenat@mail.ru>
2019-10-23 22:30:18 +0300

    Update ex4.c

--- week10/ex4.c
+++ week10/ex4.c
@@ -3,25 +3,42 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <string.h>
+#define BUF 512
 
 int main(int argc, char const *argv[])
 {	
 	struct dirent *pDirent;
-	struct stat sfile; //pointer to stat struct
+	struct stat sfile;
 	DIR *dir;
-	char name[] = "/home/renat/homeworks/week10/tmp";
-  	stat("link1", &sfile); //stat system call
-  	dir = opendir(name);
-  	printf("st_mode = %o nlink = %lu", sfile.st_mode, sfile.st_nlink); //accessing st_mode (data member of stat struct)
+	char name[] = "tmp/";
 
+  	dir = opendir(name);
     if (dir == NULL) {
         printf ("Cannot open directory '%s'\n", name);
         return 1;
     }
 
     while ((pDirent = readdir(dir)) != NULL) {
-    	stat(pDirent->d_name, &sfile);
-        printf ("[%s] %lu %lu\n", pDirent->d_name, sfile.st_nlink, sfile.st_ino);
+    	char path[BUF];
+    	strcpy(path, name);
+    	strcat(path, pDirent->d_name);
+    	stat(path, &sfile);
+        if(sfile.st_nlink > 1){
+        	printf("%s %lu\n", pDirent->d_name, sfile.st_ino);
+        	DIR *dir1;
+        	dir1 = opendir("tmp/");
+        	struct dirent *pDirent1;
+        	struct stat sfile1;
+        	while ((pDirent1 = readdir(dir1)) != NULL) {
+		        if(stat(pDirent1->d_name, &sfile1) == 0){
+		        	if(sfile.st_ino == sfile1.st_ino){
+			        	printf("%s links to %lu\n", pDirent1->d_name, sfile1.st_ino);
+			        }
+		        }
+		    }
+		    closedir(dir1);
+        }
     }
     closedir (dir);
     
